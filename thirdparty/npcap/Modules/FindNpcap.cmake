0000665acd1058fb44273ae71fbdc9e06333b1db
Florian Reimold <11774314+FlorianReimold@users.noreply.github.com>
2022-11-24 16:10:13 +0100

    Changed pcappluplus::pcappluplus and npcap::npcap targets to lowercase.
    This makes it compatible with the target names from conan.io and reflects what the readme.md had already stated.

--- thirdparty/npcap/Modules/FindNpcap.cmake
+++ thirdparty/npcap/Modules/FindNpcap.cmake
@@ -1,90 +1,90 @@
-# - Try to find Npcap include dirs and libraries
+# - Try to find npcap include dirs and libraries
 #
 # Usage of this module as follows:
 #
-#     find_package(Npcap)
+#     find_package(npcap)
 #
 # Variables used by this module, they can change the default behaviour and need
 # to be set before calling find_package:
 #
-#  Npcap_ROOT_DIR            Set this variable to the root directory of
-#                            the Npcap SDK
+#  npcap_ROOT_DIR            Set this variable to the root directory of
+#                            the npcap SDK
+#
+# Targets created by this module:
+#   
+#  npcap::npcap               Interface target for all libraries below
+#  npcap::wpcap               wpcap.lib
+#  npcap::packet              Packet.lib
 #
 # Variables defined by this module:
 #
-#  Npcap_FOUND                System has Npcap, include and library dirs found
-#  Npcap_INCLUDE_DIR          The Npcap include directories.
-#  Npcap_LIBS                 The Npcap libraries
+#  npcap_FOUND                System has npcap, include and library dirs found
+#  npcap_INCLUDE_DIR          The npcap include directories.
+#  npcap_LIBS                 The npcap libraries
+#
 
 # Root dir
-find_path(Npcap_ROOT_DIR
+find_path(npcap_ROOT_DIR
     NAMES
         Include/pcap.h
 )
 
 # Include dir
-find_path(Npcap_INCLUDE_DIR
+find_path(npcap_INCLUDE_DIR
     NAMES
         pcap.h
     HINTS
-        "${Npcap_ROOT_DIR}/Include"
+        "${npcap_ROOT_DIR}/Include"
 )
 
 # Lib dir
 if(("${CMAKE_GENERATOR_PLATFORM}" MATCHES "x64") OR ("${CMAKE_GENERATOR}" MATCHES "(Win64|IA64)"))
     # Find 64-bit libraries
-    find_path (Npcap_LIB_DIR
+    find_path (npcap_LIB_DIR
         NAMES wpcap.lib
-        HINTS "${Npcap_ROOT_DIR}/Lib/x64/"
+        HINTS "${npcap_ROOT_DIR}/Lib/x64/"
     )
 else()
     # Find 32-bit libraries
-    find_path (Npcap_LIB_DIR
+    find_path (npcap_LIB_DIR
         NAMES wpcap.lib
-        HINTS "${Npcap_ROOT_DIR}/Lib/"
+        HINTS "${npcap_ROOT_DIR}/Lib/"
     )
 endif()
 
 # Lib files
-set(Npcap_LIBS
-  "${Npcap_LIB_DIR}/Packet.lib" 
-  "${Npcap_LIB_DIR}/wpcap.lib" 
+set(npcap_LIBS
+  "${npcap_LIB_DIR}/Packet.lib" 
+  "${npcap_LIB_DIR}/wpcap.lib" 
 )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Npcap DEFAULT_MSG
-    Npcap_LIBS
-    Npcap_INCLUDE_DIR
+find_package_handle_standard_args(npcap DEFAULT_MSG
+    npcap_LIBS
+    npcap_INCLUDE_DIR
 )
 
-# if (Npcap_FOUND AND NOT TARGET Npcap::Npcap)
-  # add_library(Npcap::Npcap UNKNOWN IMPORTED)
-  # set_target_properties(Npcap::Npcap PROPERTIES
-                        # INTERFACE_INCLUDE_DIRECTORIES "${Npcap_INCLUDE_DIR}"
-                        # IMPORTED_LOCATION ${Npcap_LIBS})
-# endif ()
+if (npcap_FOUND AND NOT TARGET npcap::npcap)
 
-if (Npcap_FOUND AND NOT TARGET Npcap::Npcap)
-
-    add_library(Npcap::wpcap UNKNOWN IMPORTED)
-    set_target_properties(Npcap::wpcap PROPERTIES
-                        INTERFACE_INCLUDE_DIRECTORIES "${Npcap_INCLUDE_DIR}"
-                        IMPORTED_LOCATION "${Npcap_LIB_DIR}/wpcap.lib")
+    add_library(npcap::wpcap UNKNOWN IMPORTED)
+    set_target_properties(npcap::wpcap PROPERTIES
+                        INTERFACE_INCLUDE_DIRECTORIES "${npcap_INCLUDE_DIR}"
+                        IMPORTED_LOCATION "${npcap_LIB_DIR}/wpcap.lib")
                         
-    add_library(Npcap::Packet UNKNOWN IMPORTED)
-    set_target_properties(Npcap::Packet PROPERTIES
-                        INTERFACE_INCLUDE_DIRECTORIES "${Npcap_INCLUDE_DIR}"
-                        IMPORTED_LOCATION "${Npcap_LIB_DIR}/Packet.lib")
+    add_library(npcap::packet UNKNOWN IMPORTED)
+    set_target_properties(npcap::packet PROPERTIES
+                        INTERFACE_INCLUDE_DIRECTORIES "${npcap_INCLUDE_DIR}"
+                        IMPORTED_LOCATION "${npcap_LIB_DIR}/Packet.lib")
                         
-    add_library(Npcap::Npcap INTERFACE IMPORTED)
-    set_property(TARGET Npcap::Npcap PROPERTY
-                        INTERFACE_LINK_LIBRARIES Npcap::wpcap Npcap::Packet)
+    add_library(npcap::npcap INTERFACE IMPORTED)
+    set_property(TARGET npcap::npcap PROPERTY
+                        INTERFACE_LINK_LIBRARIES npcap::wpcap npcap::packet)
 
 endif()
 
 mark_as_advanced(
-    Npcap_ROOT_DIR
-    Npcap_INCLUDE_DIR
-    Npcap_LIB_DIR
-    Npcap_LIBS
+    npcap_ROOT_DIR
+    npcap_INCLUDE_DIR
+    npcap_LIB_DIR
+    npcap_LIBS
 )
