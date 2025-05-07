4a1883e19ec48dfd8c171c506b470b55177952b8
Florian Reimold <11774314+FlorianReimold@users.noreply.github.com>
2025-05-07 11:19:33 +0200

    [CMake] Modernized some cmake files (#17)
    - Switched to modern CMake FetchContent_MakeAvailable (it was FetchContent_Populate before, which is deprecated)
    - Updated the GTest version that is being fetched

--- thirdparty/npcap/npcap_make_available.cmake
+++ thirdparty/npcap/npcap_make_available.cmake
@@ -7,13 +7,11 @@ include(FetchContent)
 FetchContent_Declare(npcap_sdk
     URL      "${NPCAP_SDK_ARCHIVE_URL}"
     URL_HASH "${NPCAP_SDK_ARCHIVE_HASH}"
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
+    DOWNLOAD_EXTRACT_TIMESTAMP FALSE
     )
-FetchContent_GetProperties(npcap_sdk)
-if(NOT npcap-sdk_POPULATED)
-    message(STATUS "Fetching npcap_sdk...")
-    FetchContent_Populate(npcap_sdk)
-endif()
+
+message(STATUS "Fetching npcap_sdk...")
+FetchContent_MakeAvailable(npcap_sdk)
+
 set(npcap_ROOT_DIR "${npcap_sdk_SOURCE_DIR}")
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/Modules/)
